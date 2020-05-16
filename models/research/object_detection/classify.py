2a13e5aeea75719aa9cbd7c1daf49b2e6aa8bcfd
Renato Zimmermann <renatomatz@gmail.com>
2020-05-16 08:58:48 -0400

    added TF object detection and webcam classifier 2

---
+++ models/research/object_detection/classify.py
@@ -0,0 +1,168 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+# import zipfile
+
+# from collections import defaultdict
+# from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+sys.path.append("..")
+from object_detection.utils import ops as util_ops
+
+from object_detection.utils import label_map_util
+
+from object_detection.utils import visualization_utils as vis_util
+
+MODEL_NAME = "ssdlite_mobilenet_v2_coco_2018_05_09"
+MODEL_FILE = MODEL_NAME + ".tar.gz"
+DOWNLOAD_BASE = "http://download.tensorflow.org/models/object_detection/"
+
+PATH_TO_CKPT = MODEL_NAME + "/frozen_inference_graph.pb"
+
+PATH_TO_LABELS = os.path.join("data", "mscoco_label_map.pbtxt")
+
+NUM_CLASSES = 90
+
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+    file_name = os.path.basename(file.name)
+    if "frozen_inference_graph.pb" in file_name:
+        tar_file.extract(file, os.getcwd())
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, "rb") as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(
+        label_map,
+        max_num_classes=NUM_CLASSES,
+        use_display_name=True
+        )
+category_index = label_map_util.create_category_index(categories)
+
+
+def load_image_into_numpy_array(image):
+    im_width, im_height = image.size
+    return np.array(image.getdata())\
+        .reshape((im_height, im_width, 3))\
+        .astype(np.uint8)
+
+
+PATH_TO_TEST_IMAGES_DIR = "test_images"
+TEST_IMAGE_PATHS = [
+    os.path.join(
+        PATH_TO_TEST_IMAGES_DIR,
+        'image{}.jpg'.format(i)
+    )
+    for i in range(1, 3)
+]
+
+
+def run_inference_for_single_image(image, graph):
+    with graph.as_default():
+        with tf.compat.v1.Session() as sess:
+            ops = tf.compat.v1.get_default_graph().get_operations()
+            all_tensor_names = {
+                    output.name
+                    for op in ops
+                    for output in op.outputs
+                    }
+
+            tensor_dict = {}
+            for key in [
+                "num_detections", "detection_boxes",
+                "detection_scores", "detection_classes",
+                "detection_masks"
+            ]:
+                tensor_name = key + ":0"
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.compat.v1.get_default_graph()\
+                            .get_tensor_by_name(tensor_name)
+
+
+            detection_boxes = tf.squeeze(
+                    tensor_dict["detection_boxes"],
+                    [0]
+            )
+
+            real_num_detection = tf.cast(
+                    tensor_dict["num_detections"][0],
+                    tf.int32
+            )
+
+            detection_boxes = tf.slice(
+                    detection_boxes,
+                    [0, 0],
+                    [real_num_detection, -1]
+            )
+
+            image_tensor = (
+                    tf.compat.v1.get_default_graph()
+                    .get_tensor_by_name("image_tensor:0")
+            )
+
+            output_dict = sess.run(
+                    tensor_dict,
+                    feed_dict={
+                        image_tensor: np.expand_dims(image, 0)
+                    }
+            )
+
+            output_dict['num_detections'] = int(
+                    output_dict['num_detections'][0]
+            )
+
+            output_dict['detection_classes'] = (
+                    output_dict['detection_classes'][0]
+                    .astype(np.uint8)
+            )
+
+            output_dict['detection_boxes'] = (
+                    output_dict['detection_boxes'][0]
+            )
+
+            output_dict['detection_scores'] = (
+                    output_dict['detection_scores'][0]
+            )
+
+            return output_dict
+
+
+for i, image_path in enumerate(TEST_IMAGE_PATHS):
+
+    image = Image.open(image_path)
+
+    image_np = load_image_into_numpy_array(image)
+
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    output_dict = run_inference_for_single_image(
+            image_np,
+            detection_graph
+    )
+
+    image_np = vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            output_dict["detection_boxes"],
+            output_dict["detection_classes"],
+            output_dict["detection_scores"],
+            category_index,
+            instance_masks=None,
+            use_normalized_coordinates=True,
+            line_thickness=8
+    )
+
+    plt.imshow(image_np)
+    plt.show()
